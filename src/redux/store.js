d3197063ca4fade1812b24ee3b3cbf453d7120c1
sreekumar <sreekumar.d@technoduces.com>
2024-08-23 13:28:14 +0530

    final commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './slice';
+import { loadState, saveState } from "../components/localStorage";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
   },
+  preloadedState,
+});
+
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
